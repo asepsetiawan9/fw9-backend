523da67c749ee6876eb505483a6c3c36cf32f4a8
asetiawan9 <zep.setiawan09@gmail.com>
2022-07-06 17:03:53 +0700

    fix: db, erd (revisi)

--- dump-zwalet-202206281403.sql
+++ dump-zwalet-202206281403.sql
@@ -5,7 +5,31 @@
 -- Dumped from database version 14.3
 -- Dumped by pg_dump version 14.3
 
--- Started on 2022-06-28 14:03:02
+-- Started on 2022-07-06 16:22:33
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE zwalet;
+--
+-- TOC entry 3348 (class 1262 OID 24586)
+-- Name: zwalet; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE zwalet WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'English_United States.1252';
+
+
+ALTER DATABASE zwalet OWNER TO postgres;
+
+\connect zwalet
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -29,7 +53,7 @@ CREATE SCHEMA public;
 ALTER SCHEMA public OWNER TO postgres;
 
 --
--- TOC entry 3343 (class 0 OID 0)
+-- TOC entry 3349 (class 0 OID 0)
 -- Dependencies: 3
 -- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
 --
@@ -51,10 +75,9 @@ CREATE TABLE public.profile (
     fullname character varying(255),
     picture character varying(255),
     phone character varying(20),
-    id_user integer NOT NULL,
-    balance money,
-    income money,
-    expanse money
+    id_user integer,
+    expand bigint,
+    income bigint
 );
 
 
@@ -82,12 +105,14 @@ ALTER TABLE public.profile ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 CREATE TABLE public.transaction (
     id integer NOT NULL,
-    amount double precision,
+    amount bigint,
     time_date timestamp without time zone,
     note text,
-    id_user integer NOT NULL,
-    id_profile integer NOT NULL,
-    typetrans_id integer NOT NULL
+    recipient_id integer NOT NULL,
+    sender_id integer NOT NULL,
+    typetrans_id integer NOT NULL,
+    notes text,
+    profile_id integer
 );
 
 
@@ -147,7 +172,7 @@ CREATE TABLE public.users (
     username character varying(255) NOT NULL,
     email character varying(255) NOT NULL,
     password character varying(255) NOT NULL,
-    pin character varying(6)
+    pin character varying(6) NOT NULL
 );
 
 
@@ -169,27 +194,27 @@ ALTER TABLE public.users ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 3335 (class 0 OID 24665)
+-- TOC entry 3340 (class 0 OID 24665)
 -- Dependencies: 214
 -- Data for Name: profile; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.profile (id, fullname, picture, phone, id_user, balance, income, expanse) FROM stdin;
+COPY public.profile (id, fullname, picture, phone, id_user, expand, income) FROM stdin;
 \.
 
 
 --
--- TOC entry 3333 (class 0 OID 24657)
+-- TOC entry 3338 (class 0 OID 24657)
 -- Dependencies: 212
 -- Data for Name: transaction; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.transaction (id, amount, time_date, note, id_user, id_profile, typetrans_id) FROM stdin;
+COPY public.transaction (id, amount, time_date, note, recipient_id, sender_id, typetrans_id, notes, profile_id) FROM stdin;
 \.
 
 
 --
--- TOC entry 3337 (class 0 OID 32779)
+-- TOC entry 3342 (class 0 OID 32779)
 -- Dependencies: 216
 -- Data for Name: transaction_type; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -199,7 +224,7 @@ COPY public.transaction_type (id, name, description) FROM stdin;
 
 
 --
--- TOC entry 3331 (class 0 OID 24649)
+-- TOC entry 3336 (class 0 OID 24649)
 -- Dependencies: 210
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -209,43 +234,52 @@ COPY public.users (id, username, email, password, pin) FROM stdin;
 
 
 --
--- TOC entry 3344 (class 0 OID 0)
+-- TOC entry 3350 (class 0 OID 0)
 -- Dependencies: 213
 -- Name: profile_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.profile_id_seq', 1, false);
+SELECT pg_catalog.setval('public.profile_id_seq', 12, true);
 
 
 --
--- TOC entry 3345 (class 0 OID 0)
+-- TOC entry 3351 (class 0 OID 0)
 -- Dependencies: 215
 -- Name: transaction_type_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.transaction_type_id_seq', 1, false);
+SELECT pg_catalog.setval('public.transaction_type_id_seq', 2, true);
 
 
 --
--- TOC entry 3346 (class 0 OID 0)
+-- TOC entry 3352 (class 0 OID 0)
 -- Dependencies: 211
 -- Name: transactions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.transactions_id_seq', 1, false);
+SELECT pg_catalog.setval('public.transactions_id_seq', 9, true);
 
 
 --
--- TOC entry 3347 (class 0 OID 0)
+-- TOC entry 3353 (class 0 OID 0)
 -- Dependencies: 209
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_id_seq', 94, true);
 
 
 --
--- TOC entry 3184 (class 2606 OID 24671)
+-- TOC entry 3180 (class 2606 OID 32898)
+-- Name: users email_un; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT email_un UNIQUE (email);
+
+
+--
+-- TOC entry 3188 (class 2606 OID 24671)
 -- Name: profile profile_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -254,7 +288,7 @@ ALTER TABLE ONLY public.profile
 
 
 --
--- TOC entry 3186 (class 2606 OID 32785)
+-- TOC entry 3190 (class 2606 OID 32785)
 -- Name: transaction_type transaction_type_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -263,7 +297,7 @@ ALTER TABLE ONLY public.transaction_type
 
 
 --
--- TOC entry 3182 (class 2606 OID 24663)
+-- TOC entry 3186 (class 2606 OID 24663)
 -- Name: transaction transactions_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -272,7 +306,16 @@ ALTER TABLE ONLY public.transaction
 
 
 --
--- TOC entry 3180 (class 2606 OID 24655)
+-- TOC entry 3182 (class 2606 OID 32896)
+-- Name: users username_un; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT username_un UNIQUE (username);
+
+
+--
+-- TOC entry 3184 (class 2606 OID 24655)
 -- Name: users users_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -281,7 +324,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 3190 (class 2606 OID 32835)
+-- TOC entry 3195 (class 2606 OID 32835)
 -- Name: profile profile_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -290,33 +333,42 @@ ALTER TABLE ONLY public.profile
 
 
 --
--- TOC entry 3187 (class 2606 OID 32846)
--- Name: transaction transaction_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.transaction
-    ADD CONSTRAINT transaction_fk FOREIGN KEY (id_profile) REFERENCES public.profile(id);
-
-
---
--- TOC entry 3188 (class 2606 OID 32851)
+-- TOC entry 3191 (class 2606 OID 41101)
 -- Name: transaction transaction_fk_1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.transaction
-    ADD CONSTRAINT transaction_fk_1 FOREIGN KEY (id_user) REFERENCES public.users(id);
+    ADD CONSTRAINT transaction_fk_1 FOREIGN KEY (recipient_id) REFERENCES public.users(id);
 
 
 --
--- TOC entry 3189 (class 2606 OID 32856)
+-- TOC entry 3192 (class 2606 OID 41106)
 -- Name: transaction transaction_fk_2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.transaction
-    ADD CONSTRAINT transaction_fk_2 FOREIGN KEY (typetrans_id) REFERENCES public.transaction_type(id);
+    ADD CONSTRAINT transaction_fk_2 FOREIGN KEY (sender_id) REFERENCES public.users(id);
 
 
--- Completed on 2022-06-28 14:03:03
+--
+-- TOC entry 3193 (class 2606 OID 41111)
+-- Name: transaction transaction_fk_3; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.transaction
+    ADD CONSTRAINT transaction_fk_3 FOREIGN KEY (typetrans_id) REFERENCES public.transaction_type(id);
+
+
+--
+-- TOC entry 3194 (class 2606 OID 41116)
+-- Name: transaction transaction_fk_4; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.transaction
+    ADD CONSTRAINT transaction_fk_4 FOREIGN KEY (profile_id) REFERENCES public.profile(id);
+
+
+-- Completed on 2022-07-06 16:22:34
 
 --
 -- PostgreSQL database dump complete
